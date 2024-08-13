a682688a1c6f648b007d30413a2489c8adb7048d
MachineUserPTV <github@ptvgroup.com>
2024-08-13 15:22:16 +0000

    Automatic synchronisation from developer.myptv.com

---
+++ java/main/java/com/ptvgroup/developer/client/data/ServerVariable.java
@@ -0,0 +1,24 @@
+package com.ptvgroup.developer.client.data;
+
+import java.util.HashSet;
+
+/**
+ * Representing a Server Variable for server URL template substitution.
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-08-13T15:20:53.585799088Z[Etc/UTC]", comments = "Generator version: 7.7.0")
+public class ServerVariable {
+    public String description;
+    public String defaultValue;
+    public HashSet<String> enumValues = null;
+
+    /**
+     * @param description A description for the server variable.
+     * @param defaultValue The default value to use for substitution.
+     * @param enumValues An enumeration of string values to be used if the substitution options are from a limited set.
+     */
+    public ServerVariable(String description, String defaultValue, HashSet<String> enumValues) {
+        this.description = description;
+        this.defaultValue = defaultValue;
+        this.enumValues = enumValues;
+    }
+}
