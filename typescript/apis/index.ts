d8509088a82d266787b2c5515ca4d46cee6d162e
MachineUserPTV <github@ptvgroup.com>
2024-08-13 14:54:10 +0000

    Automatic synchronisation from developer.myptv.com

--- typescript/apis/index.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './CustomRoadAttributesApi';
-export * from './MapInformationApi';
-export * from './VehicleModelsApi';
-export * from './VehicleProfilesApi';
